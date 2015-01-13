fbca6577e6602ea22786ccbe88b9399a0cb6cf04
Jackie Fletcher & Sarah Calpo <student@epicodus.com>
2015-01-13 10:04:11 -0800

    add Sinatra app

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/task')
+require('./lib/to_do')
+
+get("/") do
+  @tasks = Task.all()
+  erb(:form)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  task = Task.new(description)
+  task.save()
+  erb(:success)
+end
