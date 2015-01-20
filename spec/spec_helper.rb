07c9d620fc5a716992298eb4ee2732f525cfccb7
Sarah Joy Calpo & Arsi <student@epicodus.com>
2015-01-20 11:54:05 -0800

    add List class and methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require('rspec')
 require('task')
+require('list')
 require('pg')
 
 DB = PG.connect({:dbname => 'to_do_test_db'})
@@ -7,5 +8,6 @@ DB = PG.connect({:dbname => 'to_do_test_db'})
 RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM tasks *;")
+    DB.exec("DELETE FROM lists *;")
   end
 end
